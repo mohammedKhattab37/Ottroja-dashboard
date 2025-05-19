0916b8a3df0a676a907bff74fd3173e149a096ec
Amar2Kk <amarabas9733@gmail.com>
2025-05-19 16:08:32 +0300

    Update Medusa configuration to include backend URL for admin settings

--- medusa-config.ts
+++ medusa-config.ts
@@ -20,5 +20,6 @@ module.exports = defineConfig({
     },
     admin: {
         disable: process.env.DISABLE_MEDUSA_ADMIN === "true",
+        backendUrl: process.env.MEDUSA_BACKEND_URL,
     },
 });

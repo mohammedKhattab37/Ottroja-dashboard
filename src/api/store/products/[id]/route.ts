c13b78beb10b930999d3c6afbe8085b689d4df34
Amar2Kk <amarabas9733@gmail.com>
2025-06-03 18:42:02 +0300

    Refactor product translation service resolution in GET request handler to use typed service for improved type safety and clarity.

--- src/api/store/products/[id]/route.ts
+++ src/api/store/products/[id]/route.ts
@@ -1,15 +1,15 @@
 import { MedusaRequest, MedusaResponse } from "@medusajs/framework/http";
 import { Modules } from "@medusajs/framework/utils";
 import { PRODUCT_TRANSLATION_MODULE } from "../../../../modules/product-translations";
+import ProductTranslationService from "../../../../modules/product-translations/service";
 
 export async function GET(req: MedusaRequest, res: MedusaResponse) {
   const { id } = req.params;
   const languageCode = req.headers["accept-language"] || "EN";
 
   const productModuleService = req.scope.resolve(Modules.PRODUCT);
-  const productTranslationService = req.scope.resolve(
-    PRODUCT_TRANSLATION_MODULE
-  );
+  const productTranslationService =
+      req.scope.resolve<ProductTranslationService>(PRODUCT_TRANSLATION_MODULE);
 
   const product = await productModuleService.retrieveProduct(id, {
     relations: ["variants"],

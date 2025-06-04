de2c9462465584210eb77a8b003e531422dd22b7
Amar2Kk <amarabas9733@gmail.com>
2025-06-04 15:59:27 +0300

    Add S3 configuration options to environment template and update Medusa config for S3 file storage integration

--- medusa-config.ts
+++ medusa-config.ts
@@ -3,30 +3,49 @@ import { loadEnv, defineConfig } from "@medusajs/framework/utils";
 loadEnv(process.env.NODE_ENV || "development", process.cwd());
 
 module.exports = defineConfig({
-  projectConfig: {
-    databaseUrl: process.env.DATABASE_URL,
-    redisUrl: process.env.REDIS_URL,
-    http: {
-      storeCors: process.env.STORE_CORS!,
-      adminCors: process.env.ADMIN_CORS!,
-      authCors: process.env.AUTH_CORS!,
-      jwtSecret: process.env.JWT_SECRET || "supersecret",
-      cookieSecret: process.env.COOKIE_SECRET || "supersecret",
+    projectConfig: {
+        databaseUrl: process.env.DATABASE_URL,
+        redisUrl: process.env.REDIS_URL,
+        http: {
+            storeCors: process.env.STORE_CORS!,
+            adminCors: process.env.ADMIN_CORS!,
+            authCors: process.env.AUTH_CORS!,
+            jwtSecret: process.env.JWT_SECRET || "supersecret",
+            cookieSecret: process.env.COOKIE_SECRET || "supersecret",
+        },
+        workerMode: process.env.MEDUSA_WORKER_MODE as
+            | "shared"
+            | "worker"
+            | "server",
     },
-    workerMode: process.env.MEDUSA_WORKER_MODE as
-      | "shared"
-      | "worker"
-      | "server",
-  },
-  admin: {
-    disable: process.env.DISABLE_MEDUSA_ADMIN === "true",
-    backendUrl: process.env.MEDUSA_BACKEND_URL,
-  },
-  modules: [
-    {
-      resolve: "./src/modules/product-review",
+    admin: {
+        disable: process.env.DISABLE_MEDUSA_ADMIN === "true",
+        backendUrl: process.env.MEDUSA_BACKEND_URL,
     },
-    { resolve: "./src/modules/product-translations" },
-    { resolve: "./src/modules/category-translations" },
-  ],
+    modules: [
+        {
+            resolve: "@medusajs/medusa/file",
+            options: {
+                providers: [
+                    {
+                        resolve: "@medusajs/medusa/file-s3",
+                        id: "s3",
+                        options: {
+                            file_url: process.env.S3_FILE_URL,
+                            access_key_id: process.env.S3_ACCESS_KEY_ID,
+                            secret_access_key: process.env.S3_SECRET_ACCESS_KEY,
+                            region: process.env.S3_REGION,
+                            bucket: process.env.S3_BUCKET,
+                            endpoint: process.env.S3_ENDPOINT,
+                        },
+                    },
+                ],
+            },
+        },
+        {
+            resolve: "./src/modules/product-review",
+        },
+        { resolve: "./src/modules/product-translations" },
+        { resolve: "./src/modules/category-translations" },
+    ],
 });

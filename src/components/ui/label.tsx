533ddf286d469ab674f5e6d81fb914c25837b460
Amar2Kk <amarabas9733@gmail.com>
2025-07-02 14:26:02 +0300

    fix: update import statement for LabelPrimitive to use namespace import from Radix UI

--- src/components/ui/label.tsx
+++ src/components/ui/label.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { Label as LabelPrimitive } from '@radix-ui/react-label';
+import * as LabelPrimitive from '@radix-ui/react-label';
 import type * as React from 'react';
 
 import { cn } from '@/lib/utils';
